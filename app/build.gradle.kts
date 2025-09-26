a1a5e149b322d2a07f5747c16a2c67edcf701a8c
ernos <max.cophet@gmail.com>
2025-09-26 19:21:11 +0200

    Design changes,button style and layout. Version patch.
    TODO: Auto load local keypair from storage, ask for password if needed.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.yourdev.easypgp"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.55"
+        versionCode = 3
+        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
